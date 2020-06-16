7d34e9f3c37f7032952c2dc027558e517e271127
Deepak <deepakak64@gmail.com>
2020-06-16 22:46:46 +0530

    React JS ravenous project

---
+++ React JS/ravenous/Readme.md
@@ -0,0 +1,25 @@
+# Ravenous
+
+In total, there are four parts of this project:
+-   Creating Static Components
+-   Passing Information to Components
+-   Setting the State of Ravenous Components
+-   Interacting with the Yelp API
+
+Here’s a quick overview of how Ravenous should function:
+-   As a user, I should be able to search for restaurants
+-   As a user, I should be able to view a list of restaurants returned by the Yelp API
+-   As a user, I should be able to sort through restaurants using a filter
+
+__Requirements__
+
+* Node js
+* Command Line Terminal
+
+__Quick Start__
+
+This project is already in production phase, not in development phase. So, to start this app, one can use static server and let it handle all works itself. Run these commands in any CLI from ReactJs directory.
+
+>>npm install -g serve
+
+>>serve -s ravenous
---
+++ React JS/ravenous/precache-manifest.7bf196d15f6a9dcaaca6db9f9377e050.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e1a230f195bac78343e82987ca4dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f26cbb289d7af816a2f",
+    "url": "/static/css/main.79a08113.chunk.css"
+  },
+  {
+    "revision": "5ffa5e30ba2a707bf718",
+    "url": "/static/js/2.54bd7e5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54bd7e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f26cbb289d7af816a2f",
+    "url": "/static/js/main.73d7cb8f.chunk.js"
+  },
+  {
+    "revision": "f08bef91f12dbf928030",
+    "url": "/static/js/runtime-main.6325cf30.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
